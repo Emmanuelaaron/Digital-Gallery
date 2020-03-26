102c519f586ca929673206c261970c5bec882f71
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-03-26 22:26:48 +0300

    Connects the project to the database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+database = os.getenv("db_url")
 
 def create_app(test_config=None):
   app = Flask(__name__)
+  app.config['SQLALCHEMY_DATABASE_URI'] = database
+  db = SQLAlchemy(app)
 
   @app.route('/')
   def index():
