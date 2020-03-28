ecceb537d2acc9506ca783fefaf8912144bac8af
Derrick Sekidde <kidricederek@gmail.com>
2020-03-28 23:45:33 +0300

    add cors policy

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_cors import CORS
 from config import Dev, Prod
 
 
